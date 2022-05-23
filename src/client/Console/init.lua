87ab530e6feb493a6514f54ca5588ff21c0d2ab9
Haruka <96925398+haruland@users.noreply.github.com>
2022-05-23 19:16:51 +0300

    Main console widget

---
+++ src/client/Console/init.lua
@@ -0,0 +1,53 @@
+-- Console - contact@shiroko.me - 2022/05/23
+-- Description:
+local UserInputService = game:GetService("UserInputService")
+local LogService = game:GetService("LogService")
+
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Packages = ReplicatedStorage:WaitForChild("Packages")
+
+local ConsoleComponent = require(script.Parent.Console)
+local ConsoleLog = require(script.Parent.ConsoleLog)
+local CUI = require(Packages.CUI)
+
+local Console = {
+	Holder = nil,
+}
+
+function Console:Load()
+	self.Holder = CUI:CreateElement("ScreenGui", {
+		IgnoreGuiInset = true,
+		ResetOnSpawn = false,
+		Enabled = false,
+	})
+	self.ConsoleFrame = self.Holder:Add(ConsoleComponent())
+
+	local Enabled = false
+
+	UserInputService.InputBegan:Connect(function(Input: InputObject)
+		if Input.KeyCode == Enum.KeyCode.F2 then
+			Enabled = not Enabled
+			Console:SetEnabled(Enabled)
+		end
+	end)
+
+	LogService.MessageOut:Connect(function(message, messageType)
+		self:OnLog(messageType, message)
+	end)
+
+	self.Holder:Mount(Players.LocalPlayer.PlayerGui)
+end
+
+function Console:OnLog(Type: Enum, Message: string)
+	self.ConsoleFrame:Add(ConsoleLog({
+		InfoType = Type,
+		Message = Message,
+	}))
+end
+
+function Console:SetEnabled(Enabled: boolean)
+	self.Holder:SetProperty("Enabled", Enabled)
+end
+
+return Console
