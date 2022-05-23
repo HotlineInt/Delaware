6c9aae0e04be4e141d801bc580278964fc06c0c7
Haruka <96925398+haruland@users.noreply.github.com>
2022-05-23 19:20:15 +0300

    Make it not overlap with the topbar

--- src/client/Console/Console.lua
+++ src/client/Console/Console.lua
@@ -4,18 +4,16 @@ local Packages = ReplicatedStorage:WaitForChild("Packages")
 local CUI = require(Packages.CUI)
 
 return function()
-	return CUI:CreateElement("Frame", {
+	return CUI:CreateElement("ScrollingFrame", {
 		BackgroundColor3 = Color3.new(),
 		BackgroundTransparency = 0.5,
-		Size = UDim2.new(1, 0, 1, 0),
+		AnchorPoint = Vector2.new(0, 1),
+		Position = UDim2.new(0, 0, 1, 0),
+		Size = UDim2.new(1, 0, 0.87, 0),
+		AutomaticCanvasSize = Enum.AutomaticSize.Y,
+		CanvasSize = UDim2.new(),
 		[CUI.Children] = {
-			CUI:CreateElement("ScrollingFrame", {
-				AutomaticCanvasSize = Enum.AutomaticSize.Y,
-				CanvasSize = UDim2.new(),
-				[CUI.Children] = {
-					CUI:CreateElement("UIListLayout", {}),
-				},
-			}),
+			CUI:CreateElement("UIListLayout", {}),
 		},
 	})
 end
