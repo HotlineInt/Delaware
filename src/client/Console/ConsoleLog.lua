2bde6fae02650bcb1f0b6670f169498d0c06a085
Haruka <96925398+haruland@users.noreply.github.com>
2022-05-23 19:16:43 +0300

    Console log message component

---
+++ src/client/Console/ConsoleLog.lua
@@ -0,0 +1,30 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Packages = ReplicatedStorage:WaitForChild("Packages")
+
+local CUI = require(Packages.CUI)
+
+local Colors = {
+	[Enum.MessageType.MessageError] = Color3.fromRGB(196, 13, 44),
+	[Enum.MessageType.MessageInfo] = Color3.fromRGB(180, 180, 180),
+	[Enum.MessageType.MessageOutput] = Color3.fromRGB(188, 188, 188),
+	[Enum.MessageType.MessageWarning] = Color3.fromRGB(228, 176, 33),
+}
+
+return function(Props)
+	local InfoType = Props.InfoType or Enum.MessageType.MessageOutput
+	local Message = Props.Message
+	if not CUI:RequiredProp(Message, "string") then
+		return
+	end
+
+	return CUI:CreateElement("TextLabel", {
+		BackgroundColor3 = Color3.new(),
+		BackgroundTransparency = 0.2,
+		Font = Enum.Font.Code,
+		TextSize = 15,
+		Text = Message,
+		TextXAlignment = Enum.TextXAlignment.Left,
+		TextStrokeTransparency = 0.5,
+		TextColor3 = Colors[InfoType],
+	})
+end
