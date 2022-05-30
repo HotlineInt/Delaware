b77ac7a98d6a1b12cc3ec2207cff775a34fc5ad2
Haruka <96925398+haruland@users.noreply.github.com>
2022-05-30 16:51:14 +0300

    Checkbox setting component

---
+++ src/client/Settings/SettingsWidget/SwitchesNAll/Checkbox.lua
@@ -0,0 +1,48 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Packages = ReplicatedStorage:WaitForChild("Packages")
+local CUI = require(Packages.CUI)
+
+local SettingsManager = require(script.Parent.Parent.Parent.SettingsManager)
+
+local BooleanValues = {
+	[true] = "X",
+	[false] = "",
+}
+
+return function(Setting)
+	local Switch = CUI:CreateElement("Frame", {
+		AnchorPoint = Vector2.new(1, 0.5),
+		Position = UDim2.new(0.95, 0, 0.5, 0),
+		Size = UDim2.new(0, 25, 1, 0),
+		BorderSizePixel = 0,
+		BackgroundColor3 = Color3.new(0.2, 0.2, 0.2),
+
+		[CUI.Children] = {
+			CUI:CreateElement("TextButton", {
+				-- transparent
+				Active = true,
+				Name = Setting.Name .. "Switch",
+				BackgroundTransparency = 1,
+				Text = BooleanValues[Setting.Value],
+				Font = Enum.Font.SourceSansBold,
+				-- white text
+				TextColor3 = Color3.new(1, 1, 1),
+				Size = UDim2.new(1, 0, 1, 0),
+				TextScaled = true,
+				[CUI.OnEvent("Activated")] = function(self)
+					local CurrentValue = SettingsManager:GetSetting(Setting.Name).Value
+					print(Setting.Name)
+					SettingsManager:SetSetting(Setting.Name, not CurrentValue)
+				end,
+			}),
+		},
+	})
+
+	SettingsManager:OnSettingChange(Setting.Name, function(OldValue, NewValue)
+		print("hi??")
+		local self = Switch:Get(Setting.Name .. "Switch")
+		self:SetProperty("Text", BooleanValues[NewValue])
+	end)
+
+	return Switch
+end
