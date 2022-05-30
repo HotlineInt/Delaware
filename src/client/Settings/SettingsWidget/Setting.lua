b27c0f2c5b0b701cc6585c677e3506e445983bb7
Haruka <96925398+haruland@users.noreply.github.com>
2022-05-30 16:50:54 +0300

    Setting component for the widget

---
+++ src/client/Settings/SettingsWidget/Setting.lua
@@ -0,0 +1,34 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Packages = ReplicatedStorage:WaitForChild("Packages")
+local CUI = require(Packages.CUI)
+
+local Switches = {
+	Checkbox = require(script.Parent.SwitchesNAll.Checkbox),
+}
+
+return function(Setting)
+	local Label = Setting.Name
+	local Type = "Boolean"
+
+	return CUI:CreateElement("Frame", {
+		Name = Label,
+		BackgroundTransparency = 1,
+		Size = UDim2.new(1, 0, 0, 25),
+		--AutomaticSize = Enum.AutomaticSize.Y,
+
+		[CUI.Children] = {
+			CUI:CreateElement("TextLabel", {
+				Name = Label,
+				Text = Label,
+				Font = Enum.Font.SourceSansBold,
+				BackgroundTransparency = 1,
+				TextColor3 = Color3.new(1, 1, 1),
+				TextXAlignment = Enum.TextXAlignment.Left,
+				Size = UDim2.new(1, 0, 1, 0),
+				TextSize = 24,
+			}),
+
+			Switches.Checkbox(Setting),
+		},
+	})
+end
