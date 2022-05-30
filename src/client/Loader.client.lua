2feeff30e44a96050ce19f0c8564a352ffbe09c1
Haruka <96925398+haruland@users.noreply.github.com>
2022-05-30 16:49:38 +0300

    Cleanup Client Loader and add HUD loading

--- src/client/Loader.client.lua
+++ src/client/Loader.client.lua
@@ -1,6 +1,16 @@
 script.Parent:RemoveDefaultLoadingScreen()
 local Playerlist = require(script.Parent:WaitForChild("Playerlist"))
 local Console = require(script.Parent:WaitForChild("Console"))
+local Settings = require(script.Parent:WaitForChild("Settings").SettingsWidget)
+
+local TEST = true
+local HUD = require(script.Parent.HUD)
 
 Console:Load()
 --Playerlist:Load()
+
+Settings:Load()
+
+if TEST then
+	HUD:Load()
+end
