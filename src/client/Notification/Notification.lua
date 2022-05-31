cfd3d9193116f395fe9f08344eee8fb08682c40e
Haruka <96925398+haruland@users.noreply.github.com>
2022-05-31 15:37:05 +0300

    Notification component

---
+++ src/client/Notification/Notification.lua
@@ -0,0 +1,31 @@
+local Carbon = require(game:GetService("ReplicatedStorage"):WaitForChild("Carbon"))
+local CUI = require(Carbon.UI.CUI)
+
+return function(Props)
+	local Text = Props.Text
+
+	return CUI:CreateElement("Frame", {
+		Size = UDim2.new(1, 0, 0, 0),
+		BackgroundTransparency = 1,
+		AutomaticSize = Enum.AutomaticSize.Y,
+		[CUI.Children] = {
+			CUI:CreateElement("TextLabel", {
+				AutomaticSize = Enum.AutomaticSize.X,
+				Size = UDim2.new(1, 0, 0, 0),
+				TextTransparency = 1,
+				BackgroundTransparency = 1,
+				Text = Text,
+				Font = Enum.Font.SourceSansBold,
+				TextColor3 = Color3.new(1, 1, 1),
+				TextScaled = true,
+
+				[CUI.Children] = {
+					CUI:CreateElement("UITextSizeConstraint", {
+						MaxTextSize = 20,
+						MinTextSize = 10,
+					}),
+				},
+			}),
+		},
+	})
+end
