8722d1c29d8d26ad478abeaf5e49eb42987401e8
Haruka <96925398+haruland@users.noreply.github.com>
2022-06-01 16:56:16 +0300

    Add a ray visualizer

---
+++ src/server/Visualizers/RayVisualizer.lua
@@ -0,0 +1,12 @@
+-- taken from: https://devforum.roblox.com/t/how-do-you-visualize-a-raycast-as-a-part/657972/5
+-- modified by Haruka
+
+return function(Origin: Vector3, Position: Vector3): BasePart
+	local Distance = (Origin - Position).Magnitude
+	local Part = Instance.new("Part")
+	Part.Anchored = true
+	Part.CanCollide = false
+	Part.Size = Vector3.new(0.1, 0.1, Distance)
+	Part.Parent = workspace
+	Part.CFrame = CFrame.lookAt(Origin, Position) * CFrame.new(0, 0, -Distance / 2)
+end
