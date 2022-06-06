33f3fb8a79637339cd71b53dcb20d9f7816114c9
Haruka <96925398+haruland@users.noreply.github.com>
2022-06-06 15:43:32 +0300

    Add NakoProfile support

--- src/server/Services/TagService.lua
+++ src/server/Services/TagService.lua
@@ -1,12 +1,13 @@
 local Carbon = require(game:GetService("ReplicatedStorage"):WaitForChild("Carbon"))
-local Enum = require(Carbon.Util.Enum)
-
 local Players = game:GetService("Players")
 local ValidTags = {
 	Supporter = "tag_supporter",
 	Debugger = "tag_debugger",
 	Moderator = "tag_mod",
 }
+local NakoProfile = require(script.Parent.Parent.Data.NakoProfile)
+local PlayerData = NakoProfile:GetStore("PlayerData")
+
 local TagService = {
 	Name = "TagService",
 	Tags = ValidTags,
@@ -16,12 +17,22 @@ local TagService = {
 	},
 }
 
+local InStudio = Carbon:IsStudio()
+
 function TagService:KnitStart()
 	Players.PlayerAdded:Connect(function(Player)
 		self.PlayerTags[Player] = {}
-		self:ApplyTag(Player, self.Tags.Debugger)
-		self:ApplyTag(Player, self.Tags.Supporter)
-		self:ApplyTag(Player, self.Tags.Moderator)
+		if InStudio == false then
+			local UserProfile = PlayerData:GetProfile(Player.UserId)
+
+			for _, Tag in pairs(UserProfile.Data.Tags) do
+				self:ApplyTag(Player, Tag)
+			end
+		else
+			for _, Tag in pairs(ValidTags) do
+				self:ApplyTag(Player, Tag)
+			end
+		end
 	end)
 end
 
@@ -42,9 +53,21 @@ function TagService.Client:HasTag(Player: Player, Tag: string)
 	return TagService:PlayerHasTag(Player, Tag)
 end
 
+function TagService:SyncData(Player: Player)
+	if InStudio then
+		return
+	end -- Don't sync data in studio.
+
+	local UserTags = self.PlayerTags[Player]
+	local Profile = PlayerData:GetProfile(Player.UserId)
+
+	Profile.Data.Tags = UserTags
+end
+
 function TagService:ApplyTag(Player: Player, Tag: string)
 	local UserTags = self.PlayerTags[Player]
 	table.insert(UserTags, Tag)
+	self:SyncData()
 end
 
 function TagService:RemoveTag(Player: Player, Tag: string)
@@ -53,6 +76,7 @@ function TagService:RemoveTag(Player: Player, Tag: string)
 
 	if Index then
 		table.remove(UserTags, Index)
+		self:SyncData()
 	end
 end
 
