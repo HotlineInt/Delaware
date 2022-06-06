d5976f615ba39b52a8f2239467d3da9e533eb994
Haruka <96925398+haruland@users.noreply.github.com>
2022-06-06 15:44:10 +0300

    Load NakoProfile on server startup

--- src/server/Loader.server.lua
+++ src/server/Loader.server.lua
@@ -4,10 +4,16 @@ local ComponentLoader = require(ReplicatedStorage:WaitForChild("ComponentLoader"
 local Carbon = require(game:GetService("ReplicatedStorage"):WaitForChild("Carbon"))
 local Knit = require(Carbon.Framework.Knit)
 
+local Data = script.Parent:WaitForChild("Data")
+local NakoProfile = require(Data.NakoProfile)
+
 local ServicesFolder = script.Parent.Services
 
 local GameVersion = "v0.2a"
 workspace:SetAttribute("GameVersion", GameVersion)
+
+warn("Data initialization..")
+local PlayerStore = NakoProfile:MakeStore("PlayerData", NakoProfile.Templates.Player)
 
 local TotalServices = 0
 for _, Service: ModuleScript in pairs(ServicesFolder:GetChildren()) do
