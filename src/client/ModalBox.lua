7f101b622201506cc81282533ced9cff81a82d77
Haruka <96925398+haruland@users.noreply.github.com>
2022-06-12 01:07:35 +0300

    Add CUI component for blocking input outside of a window

---
+++ src/client/ModalBox.lua
@@ -0,0 +1,13 @@
+local Carbon = require(game:GetService("ReplicatedStorage"):WaitForChild("Carbon"))
+local CUI = require(Carbon.UI.CUI)
+
+return function(Props: {})
+	return CUI:CreateElement("TextButton", {
+		Active = false,
+		Size = UDim2.new(1, 0, 1, 0),
+		TextTransparency = 1,
+		BackgroundTransparency = 1,
+		Modal = true,
+		Visible = true,
+	})
+end
