ce667f26936d63c74d5e821a68dc36a4577f6386
Haruka <96925398+haruland@users.noreply.github.com>
2022-06-13 16:26:08 +0300

    Add a generic page template

--- src/client/Core/F4Menu/Components/Page.lua
+++ src/client/Core/F4Menu/Components/Page.lua
@@ -2,9 +2,12 @@ local Carbon = require(game:GetService("ReplicatedStorage"):WaitForChild("Carbon
 local CUI = require(Carbon.UI.CUI)
 
 return function(Props: {})
-	return CUI:CreateElement("Frame", {
+	return CUI:CreateElement("ScrollingFrame", {
 		BackgroundColor3 = Color3.new(0, 0, 0),
 		Size = UDim2.new(1, 0, 1, 0),
+		CanvasSize = UDim2.new(0, 0, 0, 0),
+		AutomaticCanvasSize = Enum.AutomaticSize.Y,
+		ScrollBarThickness = 3,
 		[CUI.Children] = Props,
 	})
 end
