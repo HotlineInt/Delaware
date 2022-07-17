b98a70b26ea2e822c0c38e7d1469739c4f34706d
Haruka <contact@shiroko.me>
2022-07-17 17:48:11 +0300

    Add name to ModalBox so it can be ref'd with :Get()

--- src/client/ModalBox.lua
+++ src/client/ModalBox.lua
@@ -3,6 +3,7 @@ local CUI = require(Carbon.UI.CUI)
 
 return function(Props: {})
 	return CUI:CreateElement("TextButton", {
+		Name = "ModalBox",
 		Active = false,
 		Size = UDim2.new(1, 0, 1, 0),
 		TextTransparency = 1,
