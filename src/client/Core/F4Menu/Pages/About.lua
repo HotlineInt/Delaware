81a494f902d5e7f9babeb59cad3d8922f4f0255e
Haruka <contact@shiroko.me>
2022-07-18 12:56:58 +0300

    Improve About page

--- src/client/Core/F4Menu/Pages/About.lua
+++ src/client/Core/F4Menu/Pages/About.lua
@@ -2,19 +2,24 @@ local Carbon = require(game:GetService("ReplicatedStorage"):WaitForChild("Carbon
 local CUI = require(Carbon.UI.CUI)
 
 local Page = require(script.Parent.Parent.Components.Page)
+local Label = require(script.Parent.Parent.Components.Label)
+
+local GameVersion = workspace:GetAttribute("GameVersion")
 
 return function(Props: {})
 	return Page({
-		CUI:CreateElement("TextLabel", {
-			Size = UDim2.new(1, 0, 1, 0),
-			BackgroundTransparency = 1,
-			RichText = true,
+		CUI:CreateElement("UIListLayout", {
+			SortOrder = Enum.SortOrder.LayoutOrder,
+			FillDirection = Enum.FillDirection.Vertical,
+			Padding = UDim.new(0, 20),
+		}),
+		Label({
 			Text = "<b>Codename: Delaware</b>\n<i>pre-release</i>",
-			Font = Enum.Font.SourceSansLight,
-			TextSize = 18,
-			TextWrapped = true,
-			TextColor3 = Color3.new(1, 1, 1),
-			TextYAlignment = Enum.TextYAlignment.Top,
+			Font = Enum.Font.SourceSans,
+		}),
+		Label({
+			Text = "Version: " .. GameVersion .. " Running Carbon v0.2",
+			Font = Enum.Font.SourceSans,
 		}),
 	})
 end
